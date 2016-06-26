140e9d0d7122ea5ac879a7e56b408ec4876410cd
Upasana Mehta <umehta@expedia.com>
2016-06-26 22:27:37 +0530

    added get_urls.js

---
+++ get_urls.js
@@ -0,0 +1,51 @@
+chrome.commands.onCommand.addListener(function(command) {
+	if(command == 'open facebook'){
+ 	
+ 		chrome.tabs.query({title: "Facebook"}, function(tabs){
+   if (tabs.length){
+       console.log("found");
+       chrome.windows.update(tabs[0].windowId , {focused : true} , function(){
+       	chrome.tabs.update(tabs[0].id, {selected: true});
+       });
+       
+   }
+    else {
+       var facebookURL = "https://www.facebook.com/";
+       chrome.tabs.create({ url: facebookURL });
+   }
+});
+	}
+	else if(command == 'open youtube'){
+
+        chrome.tabs.query({title: "*YouTube*"}, function(tabs){
+            if (tabs.length){
+                console.log("found");
+                chrome.windows.update(tabs[0].windowId , {focused : true} , function(){
+                chrome.tabs.update(tabs[0].id, {selected: true});
+            });
+            }
+            else {
+                var youtubeURL = "https://www.youtube.com/";
+                chrome.tabs.create({ url: youtubeURL });
+            }
+        });
+
+    }
+		else if(command == 'open quora'){
+				
+ 		chrome.tabs.query({title: "*Quora*"}, function(tabs){
+   if (tabs.length){
+       console.log("found");
+       chrome.windows.update(tabs[0].windowId , {focused : true} , function(){
+       chrome.tabs.update(tabs[0].id, {selected: true});
+   });
+   }
+    else {
+       var quoraURL = "https://www.quora.com/";
+       chrome.tabs.create({ url: quoraURL });
+   }
+});
+} 
+
+});
+
